3de33443256a9c28d528687a4b1ac347bdbe7ad4
Tanzim Mokammel <mtanzim@gmail.com>
2023-07-16 16:49:20 -0400

    fmt

--- 125.valid-palindrome.ts/125.valid-palindrome.ts
+++ 125.valid-palindrome.ts/125.valid-palindrome.ts
@@ -20,25 +20,19 @@
  *
  * Given a string s, return true if it is a palindrome, or false otherwise.
  *
- *
  * Example 1:
- *
  *
  * Input: s = "A man, a plan, a canal: Panama"
  * Output: true
  * Explanation: "amanaplanacanalpanama" is a palindrome.
  *
- *
  * Example 2:
- *
  *
  * Input: s = "race a car"
  * Output: false
  * Explanation: "raceacar" is not a palindrome.
  *
- *
  * Example 3:
- *
  *
  * Input: s = " "
  * Output: true
@@ -47,15 +41,10 @@
  * Since an empty string reads the same forward and backward, it is a
  * palindrome.
  *
- *
- *
  * Constraints:
- *
  *
  * 1 <= s.length <= 2 * 10^5
  * s consists only of printable ASCII characters.
- *
- *
  */
 
 // @lc code=start
--- 217.contains-duplicate.ts/217.contains-duplicate.ts
+++ 217.contains-duplicate.ts/217.contains-duplicate.ts
@@ -16,7 +16,6 @@
  * Given an integer array nums, return true if any value appears at least twice
  * in the array, and return false if every element is distinct.
  *
- *
  * Example 1:
  * Input: nums = [1,2,3,1]
  * Output: true
@@ -27,14 +26,10 @@
  * Input: nums = [1,1,1,3,3,4,3,2,4,2]
  * Output: true
  *
- *
  * Constraints:
- *
  *
  * 1 <= nums.length <= 10^5
  * -10^9 <= nums[i] <= 10^9
- *
- *
  */
 
 // @lc code=start
@@ -43,4 +38,4 @@ function containsDuplicate(nums: number[]): boolean {
   return set.size !== nums.length;
 }
 // @lc code=end
-export default containsDuplicate
+export default containsDuplicate;
