aa4d99c0f62dd4f728ba89dfd073c7cefb2c8a83
Tanzim Mokammel <mtanzim@gmail.com>
2023-11-09 10:24:25 -0500

    linked list cycle

--- 200.number-of-islands.ts/200.number-of-islands.ts
+++ 200.number-of-islands.ts/200.number-of-islands.ts
@@ -20,9 +20,7 @@
  * horizontally or vertically. You may assume all four edges of the grid are
  * all surrounded by water.
  *
- *
  * Example 1:
- *
  *
  * Input: grid = [
  * ⁠ ["1","1","1","1","0"],
@@ -32,9 +30,7 @@
  * ]
  * Output: 1
  *
- *
  * Example 2:
- *
  *
  * Input: grid = [
  * ⁠ ["1","1","0","0","0"],
@@ -44,17 +40,12 @@
  * ]
  * Output: 3
  *
- *
- *
  * Constraints:
- *
  *
  * m == grid.length
  * n == grid[i].length
  * 1 <= m, n <= 300
  * grid[i][j] is '0' or '1'.
- *
- *
  */
 
 // @lc code=start
@@ -99,16 +90,14 @@ const getGridLimits = (grid: string[][]): Limits => {
   return { maxRow, maxCol, minCol, minRow };
 };
 
-const getCoordValidator =
-  (limits: Limits) =>
-  ({ rowIdx, colIdx }: Coord) => {
-    return (
-      rowIdx >= limits.minRow &&
-      rowIdx <= limits.maxRow &&
-      colIdx >= limits.minCol &&
-      colIdx <= limits.maxCol
-    );
-  };
+const getCoordValidator = (limits: Limits) => ({ rowIdx, colIdx }: Coord) => {
+  return (
+    rowIdx >= limits.minRow &&
+    rowIdx <= limits.maxRow &&
+    colIdx >= limits.minCol &&
+    colIdx <= limits.maxCol
+  );
+};
 
 export function numIslands(grid: string[][]): number {
   const coordValidator = getCoordValidator(getGridLimits(grid));
---
+++ 206.reverse-linked-list.ts/206.reverse-linked-list.ts
@@ -0,0 +1,58 @@
+/*
+ * @lc app=leetcode id=206 lang=typescript
+ *
+ * [206] Reverse Linked List
+ *
+ * https://leetcode.com/problems/reverse-linked-list/description/
+ *
+ * algorithms
+ * Easy (74.71%)
+ * Likes:    19648
+ * Dislikes: 357
+ * Total Accepted:    3.4M
+ * Total Submissions: 4.6M
+ * Testcase Example:  '[1,2,3,4,5]'
+ *
+ * Given the head of a singly linked list, reverse the list, and return the
+ * reversed list.
+ *
+ * Example 1:
+ *
+ * Input: head = [1,2,3,4,5]
+ * Output: [5,4,3,2,1]
+ *
+ * Example 2:
+ *
+ * Input: head = [1,2]
+ * Output: [2,1]
+ *
+ * Example 3:
+ *
+ * Input: head = []
+ * Output: []
+ *
+ * Constraints:
+ *
+ * The number of nodes in the list is the range [0, 5000].
+ * -5000 <= Node.val <= 5000
+ *
+ * Follow up: A linked list can be reversed either iteratively or recursively.
+ * Could you implement both?
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function reverseList(head: ListNode | null): ListNode | null {
+}
+// @lc code=end
