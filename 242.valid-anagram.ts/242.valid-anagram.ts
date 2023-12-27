44cffb8a8237cb919307ae3b8f78468bedaef2bc
Tanzim Mokammel <mtanzim@gmail.com>
2023-12-26 17:51:48 -0700

    valid anagram

---
+++ 242.valid-anagram.ts/242.valid-anagram.ts
@@ -0,0 +1,69 @@
+/*
+ * @lc app=leetcode id=242 lang=typescript
+ *
+ * [242] Valid Anagram
+ *
+ * https://leetcode.com/problems/valid-anagram/description/
+ *
+ * algorithms
+ * Easy (63.49%)
+ * Likes:    11468
+ * Dislikes: 363
+ * Total Accepted:    3M
+ * Total Submissions: 4.7M
+ * Testcase Example:  '"anagram"\n"nagaram"'
+ *
+ * Given two strings s and t, return true if t is an anagram of s, and false
+ * otherwise.
+ *
+ * An Anagram is a word or phrase formed by rearranging the letters of a
+ * different word or phrase, typically using all the original letters exactly
+ * once.
+ *
+ * Example 1:
+ * Input: s = "anagram", t = "nagaram"
+ * Output: true
+ * Example 2:
+ * Input: s = "rat", t = "car"
+ * Output: false
+ *
+ * Constraints:
+ *
+ * 1 <= s.length, t.length <= 5 * 10^4
+ * s and t consist of lowercase English letters.
+ *
+ * Follow up: What if the inputs contain Unicode characters? How would you
+ * adapt your solution to such a case?
+ */
+
+// @lc code=start
+function isAnagram(s: string, t: string): boolean {
+  function makeHm(v: string): Record<string, number> {
+    return v.split("").reduce((acc, cur) => {
+      if (cur in acc) {
+        return {
+          ...acc,
+          [cur]: acc[cur] + 1,
+        };
+      }
+      return {
+        ...acc,
+        [cur]: 1,
+      };
+    }, {} as Record<string, number>);
+  }
+  const [shm, thm] = [makeHm(s), makeHm(t)];
+  for (const k in shm) {
+    if (shm[k] !== thm[k]) {
+      return false;
+    }
+  }
+  for (const k in thm) {
+    if (thm[k] !== shm[k]) {
+      return false;
+    }
+  }
+  return true;
+}
+// @lc code=end
+export { isAnagram };
