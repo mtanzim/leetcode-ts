85b82d1ed2b9565ff37a9a11fc03f1481bef4957
Tanzim Mokammel <mtanzim@gmail.com>
2024-01-01 20:01:08 -0700

    working on connected components

---
+++ 3651-connected-components.js
@@ -0,0 +1,69 @@
+// https://www.lintcode.com/problem/3651/
+
+import { assertEquals } from "https://deno.land/std@0.106.0/testing/asserts.ts";
+
+export class Solution {
+  /**
+   * @param n: the number of vertices
+   * @param edges: the edges of undirected graph
+   * @return: the number of connected components
+   */
+
+  makeAdj(n, edges) {
+    const adjList = [];
+    for (let i = 0; i < n; i++) {
+      adjList[i] = [];
+    }
+    // undirected graph
+    edges.forEach(([from, to]) => {
+      adjList[from].push(to);
+      adjList[to].push(from);
+    });
+    return adjList;
+  }
+
+  bfs(adj, source, visited) {
+    if (visited.has(source)) {
+      return 0;
+    }
+    const q = [source];
+    while (q.length > 0) {
+      console.log({ q });
+      const curNode = q.shift();
+      if (visited.has(curNode)) {
+        continue;
+      }
+      visited.add(String(curNode));
+      const neighbors = adj[curNode];
+      neighbors.forEach((n) => {
+        if (visited.has(n)) {
+          return;
+        }
+        q.push(n);
+      });
+    }
+    return 1;
+  }
+
+  countComponents(n, edges) {
+    const adj = this.makeAdj(n, edges);
+    const visted = new Set();
+    console.log({ adj });
+    let connectedComponents = 0;
+    for (let i = 0; i < n; i++) {
+      connectedComponents += this.bfs(adj, i, visted);
+    }
+    return connectedComponents;
+  }
+}
+
+Deno.test("test", () => {
+  const n = 3;
+  const e = [
+    [0, 1],
+    [0, 2],
+  ];
+  const output = new Solution().countComponents(n, e);
+  const expected = 1;
+  assertEquals(output, expected);
+});
