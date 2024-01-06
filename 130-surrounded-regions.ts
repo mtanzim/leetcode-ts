38f31178499a9d15ca74fc1c2a191e3f0f9b91fd
Tanzim Mokammel <mtanzim@gmail.com>
2024-01-06 16:30:28 -0500

    surrounded regions solution: fixed bugs

--- 130-surrounded-regions.ts
+++ 130-surrounded-regions.ts
@@ -17,7 +17,9 @@ function solve(board: string[][]): void {
   board.forEach((row, ri) => {
     row.forEach((v, ci) => {
       if (
-        v === "O" && (ri === board.length - 1 || ci === board[0].length - 1)
+        v === "O" &&
+        (ri === board.length - 1 || ci === board[0].length - 1 || ri === 0 ||
+          ci === 0)
       ) {
         stack.push({ ri, ci });
       }
@@ -31,20 +33,24 @@ function solve(board: string[][]): void {
     if (!cur) {
       throw Error("invalid dfs case");
     }
-    if (visited.has(toS(cur))) {
-      continue;
-    }
     visited.add(toS(cur));
     const neighbors = deltas.map(([rd, cd]) => ({
       ri: cur.ri + rd,
       ci: cur.ci + cd,
-    })).filter(({ ri, ci }) => board?.[ri]?.[ci] === "O");
+    })).filter(({ ri, ci }) => board?.[ri]?.[ci] === "O")
+      .filter((n) => !visited.has(toS(n)));
     neighbors.forEach((n) => stack.push(n));
   }
 
-  // flip everything except visited
+  // flip everything except visited on the **inside**
   board.forEach((row, ri) => {
+    if (ri === 0 || ri === board.length - 1) {
+      return;
+    }
     row.forEach((_v, ci) => {
+      if (ci === 0 || ci === board[0].length - 1) {
+        return;
+      }
       if (!visited.has(toS({ ri, ci }))) {
         board[ri][ci] = "X";
       }
@@ -65,6 +71,42 @@ Deno.test("basic", () => {
     "X",
     "X",
   ], ["X", "O", "X", "X"]];
+  solve(board);
+  assertEquals(board, expected);
+});
+
+Deno.test("failing", () => {
+  const board = [["X", "O", "X"], ["O", "X", "O"], ["X", "O", "X"]];
+  const expected = [["X", "O", "X"], ["O", "X", "O"], ["X", "O", "X"]];
+  solve(board);
+  assertEquals(board, expected);
+});
+
+Deno.test("failing 2", () => {
+  const board = [
+    ["X", "O", "O", "X", "X", "X", "O", "X", "O", "O"],
+    ["X", "O", "X", "X", "X", "X", "X", "X", "X", "X"],
+    ["X", "X", "X", "X", "O", "X", "X", "X", "X", "X"],
+    ["X", "O", "X", "X", "X", "O", "X", "X", "X", "O"],
+    ["O", "X", "X", "X", "O", "X", "O", "X", "O", "X"],
+    ["X", "X", "O", "X", "X", "O", "O", "X", "X", "X"],
+    ["O", "X", "X", "O", "O", "X", "O", "X", "X", "O"],
+    ["O", "X", "X", "X", "X", "X", "O", "X", "X", "X"],
+    ["X", "O", "O", "X", "X", "O", "X", "X", "O", "O"],
+    ["X", "X", "X", "O", "O", "X", "O", "X", "X", "O"],
+  ];
+  const expected = [
+    ["X", "O", "O", "X", "X", "X", "O", "X", "O", "O"],
+    ["X", "O", "X", "X", "X", "X", "X", "X", "X", "X"],
+    ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X"],
+    ["X", "X", "X", "X", "X", "X", "X", "X", "X", "O"],
+    ["O", "X", "X", "X", "X", "X", "X", "X", "X", "X"],
+    ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X"],
+    ["O", "X", "X", "X", "X", "X", "X", "X", "X", "O"],
+    ["O", "X", "X", "X", "X", "X", "X", "X", "X", "X"],
+    ["X", "X", "X", "X", "X", "X", "X", "X", "O", "O"],
+    ["X", "X", "X", "O", "O", "X", "O", "X", "X", "O"],
+  ];
   solve(board);
   assertEquals(board, expected);
 });
